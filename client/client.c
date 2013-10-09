8ff85384ac6e289ad0c2a75c0341ff98ec3806e5
Volatilexian <x125924@163.com>
2013-10-09 10:45:55 +0800

    it can work, even though it is very simple

--- client/client.c
+++ client/client.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <errno.h>
 #include <netdb.h>
 #include <string.h>
 #include <strings.h>
@@ -16,32 +20,33 @@ int main(int argc, char *argv[])
 	struct sockaddr_in	ser_addr;
 	char buffer[BUFSIZE];
 	int addrlen = 0;
-	int port = PORT;
-
-	struct hostent *host = (struct hostent *)malloc(sizeof(struct hostent));
-	host = gethostbyname("localhost");
-
+	in_port_t port = PORT;
+	char addr_p[INET_ADDRSTRLEN];
 
 	bzero(&ser_addr, sizeof(struct sockaddr_in));
 	ser_addr.sin_family = AF_INET;
-	ser_addr.sin_addr = *((struct in_addr *)host->h_addr);
+	inet_pton(AF_INET, "127.0.0.1", &ser_addr.sin_addr.s_addr);
 	ser_addr.sin_port = htons(port);
 
 	if(-1 == (fd = socket(ser_addr.sin_family, SOCK_DGRAM, 0)))
 		oops("create socket failed\n");
 
-	printf("the client is ready now\n");
+	inet_ntop(AF_INET, &ser_addr.sin_addr.s_addr, addr_p, sizeof(addr_p));
+	printf("ip: %s, port: %d\n", addr_p, ntohs(ser_addr.sin_port));
+	printf("the client is ready now\n\n");
 
 	printf("input what u want to say: ");
 	scanf("%s", buffer);
 	addrlen = sizeof(struct sockaddr_in);
-	if(sendto(fd, buffer, strlen(buffer)+1, 0, (struct sockaddr *)&ser_addr, sizeof(struct sockaddr)) == -1)
+	if(sendto(fd, buffer, strlen(buffer)+1, 0, (struct sockaddr *)&ser_addr, addrlen) == -1)
 		oops("send message failed\n");
 
 	if(recvfrom(fd, buffer, BUFSIZE, 0, (struct sockaddr *)&ser_addr, &addrlen) == -1)
 		oops("recevie the statu failed\n");
 
 	printf("the statu: %s\n", buffer);
+	inet_ntop(AF_INET, &ser_addr.sin_addr.s_addr, addr_p, sizeof(addr_p));
+	printf("the server ip: %s, port: %d\n\n", addr_p, ntohs(ser_addr.sin_port));
 	close(fd);
 
 	return 0;
--- server/server.c
+++ server/server.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
@@ -15,20 +18,18 @@
 
 int main(int argc, char *argv[])
 {
-	int port = PORT;
+	in_port_t port = PORT;
 	int fd;
 	struct sockaddr_in ser_addr, cln_addr;
 	char buffer[BUFSIZE];
-	int addrlen = 0;
+	socklen_t addrlen = 0;
 	int nbyte = 0;
+	char addr_p[INET_ADDRSTRLEN];
 
-	struct hostent *host = (struct hostent *)malloc(sizeof(struct hostent));
-	host = gethostbyname("localhost");
-	
 	bzero(&ser_addr, sizeof(struct sockaddr));
 	bzero(&cln_addr, sizeof(struct sockaddr));
 	ser_addr.sin_family = AF_INET;
-	ser_addr.sin_addr = *((struct in_addr *)host->h_addr);
+	ser_addr.sin_addr.s_addr = INADDR_ANY;
 	ser_addr.sin_port=htons(port);
 
 	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
@@ -37,21 +38,22 @@ int main(int argc, char *argv[])
 	if(bind(fd, (struct sockaddr *)&ser_addr, sizeof(struct sockaddr)) == -1)
 		oops("bind the socket failed\n");
 
-	printf("the server is ready now\n");
+	inet_ntop(AF_INET, &ser_addr.sin_addr.s_addr, addr_p, sizeof(addr_p));
+	printf("ip: %s, port: %d\n", addr_p, ntohs(ser_addr.sin_port));
+	printf("the server is ready now\n\n");
+
 	while(1)
 	{
+		addrlen = sizeof(cln_addr);
 		if((nbyte = recvfrom(fd, buffer, BUFSIZE, 0, (struct sockaddr *)&cln_addr, &addrlen)) < 0)
 			oops("recevie failed\n");
-		printf("this is from client: %s\n", buffer);
 
+		inet_ntop(AF_INET, &cln_addr.sin_addr.s_addr, addr_p, sizeof(addr_p));
+		printf("client: ip: %s, port: %d\n", addr_p, cln_addr.sin_port);
+		printf("message: %s\n", buffer);
 
-	printf("%d\n", nbyte);
-	printf("%d\n", cln_addr.sin_family);
-	printf("%s\n", inet_ntoa(cln_addr.sin_addr));
-	printf("%d\n", cln_addr.sin_port);
-
-		strcpy(buffer, "it's done");
-		if(sendto(fd, buffer, strlen(buffer)+1, 0, (struct sockaddr *)&cln_addr, sizeof(struct sockaddr)) == -1)
+		strcpy(buffer, DONE);
+		if(sendto(fd, buffer, strlen(buffer)+1, 0, (struct sockaddr *)&cln_addr, addrlen) == -1)
 			oops("send the result failed\n");
 	}
 
